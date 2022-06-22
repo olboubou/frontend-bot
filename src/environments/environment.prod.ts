afff57002d15626f5e45670e8015e032c5b5212d
olboubou <olive_boudj@hotmaail.fr>
2022-06-22 19:49:53 +0200

    removed skash

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  bitcoinUrl: 'https://pedroliv-crypto-trading-bot.herokuapp.com//bitcoin',
-  monargentUrl: 'https://pedroliv-crypto-trading-bot.herokuapp.com//monargent'
+  bitcoinUrl: 'https://pedroliv-crypto-trading-bot.herokuapp.com/bitcoin',
+  monargentUrl: 'https://pedroliv-crypto-trading-bot.herokuapp.com/monargent'
 };
